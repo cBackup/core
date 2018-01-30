f7c9451523fdb4982e1cddb6b90bd659b57b8d24
Olegs Capligins <oleg@dautkom.lv>
2018-01-30 10:53:27 +0200

    sys_description field in `node` also enlarged to 1Kb

--- migrations/m180130_060558_sysdescrtext.php
+++ migrations/m180130_060558_sysdescrtext.php
@@ -11,6 +11,7 @@ class m180130_060558_sysdescrtext extends Migration
         $this->dropIndex('ix_snmp', '{{%device_attributes_unknown}}');
         $this->alterColumn('{{%device_attributes}}', 'sys_description', 'VARCHAR(1024) NULL DEFAULT NULL');
         $this->alterColumn('{{%device_attributes_unknown}}', 'sys_description', 'VARCHAR(1024) NULL DEFAULT NULL');
+        $this->alterColumn('{{%node}}', 'sys_description', 'VARCHAR(1024) NULL DEFAULT NULL');
         $this->createIndex('ix_snmp', '{{%device_attributes}}', ['sysobject_id', 'hw']);
         $this->createIndex('ix_snmp', '{{%device_attributes_unknown}}', ['sysobject_id', 'hw']);
         $this->createIndex('ix_descr', '{{%device_attributes}}', ['sys_description']);
@@ -25,6 +26,7 @@ class m180130_060558_sysdescrtext extends Migration
         $this->dropIndex('ix_descr', '{{%device_attributes_unknown}}');
         $this->alterColumn('{{%device_attributes}}', 'sys_description', 'VARCHAR(255) NULL DEFAULT NULL');
         $this->alterColumn('{{%device_attributes_unknown}}', 'sys_description', 'VARCHAR(255) NULL DEFAULT NULL');
+        $this->alterColumn('{{%node}}', 'sys_description', 'VARCHAR(255) NULL DEFAULT NULL');
         $this->createIndex('ix_snmp', '{{%device_attributes}}', ['sysobject_id', 'hw', 'sys_description'], true);
         $this->createIndex('ix_snmp', '{{%device_attributes_unknown}}', ['sysobject_id', 'hw', 'sys_description'], true);
     }
